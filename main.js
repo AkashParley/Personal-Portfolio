46a4ef5eec92953a2593a577134f42a24f973a57
Akash Parle <akashparle786@gmail.com>
2024-08-02 16:05:25 +0530

    Personal-Portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "DSA Enthsiast", "Web Developer" , "UX/UI Designer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
